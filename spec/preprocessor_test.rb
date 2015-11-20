e3373a99f12f58ce9f0afa4f60a4c15e47512df6
ToniRib <tonimarierib@gmail.com>
2015-11-20 16:39:01 -0700

    test: Preprocessor byte_to_binary and word_to_binary

--- spec/preprocessor_test.rb
+++ spec/preprocessor_test.rb
@@ -27,4 +27,34 @@ class PreprocessorTest < Minitest::Test
   def test_word_to_binary_returns_empty_string_for_empty_string
     assert_equal '', @p.word_to_binary('')
   end
+
+  def test_converts_lower_case_letter_to_binary
+    expected = '01100001'
+
+    assert_equal expected, @p.word_to_binary('a')
+  end
+
+  def test_converts_different_lower_case_letter_to_binary
+    expected = '01111010'
+
+    assert_equal expected, @p.word_to_binary('z')
+  end
+
+  def test_converts_upper_case_letter_to_binary
+    expected = '01000100'
+
+    assert_equal expected, @p.word_to_binary('D')
+  end
+
+  def test_converts_different_upper_case_letter_to_binary
+    expected = '01010110'
+
+    assert_equal expected, @p.word_to_binary('V')
+  end
+
+  def test_converts_entire_string_to_binary
+    expected = '011000010110001001100011'
+
+    assert_equal expected, @p.word_to_binary('abc')
+  end
 end
