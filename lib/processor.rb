a290f35fe2418675df5aafe46b85f25b0dc5dac9
ToniRib <tonimarierib@gmail.com>
2015-11-30 14:22:31 -0700

    feat: creates message schedule as a hash

--- lib/processor.rb
+++ lib/processor.rb
@@ -13,4 +13,21 @@ class Processor
 
     (left.to_i(2) ^ right.to_i(2)).to_s(2).rjust(left.length, '0')
   end
+
+  def generate_schedule(message)
+    schedule = {}
+
+    message.each do |block|
+      80.times do |i|
+
+        if i < 16
+          schedule['t' + i.to_s] = block.chars.each_slice(32).to_a[i].join
+        else
+          schedule['t' + i.to_s] = circular_left_shift(bitwise_exclusive_or([schedule['t' + (i - 3).to_s], schedule['t' + (i - 8).to_s], schedule['t' + (i - 14).to_s], schedule['t' + (i - 16).to_s]]), 1)
+        end
+      end
+    end
+
+    schedule
+  end
 end
