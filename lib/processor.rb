671a43c88d79d00c1ca29e3bc9b0d5d1b0700f9b
ToniRib <tonimarierib@gmail.com>
2015-11-30 10:51:50 -0700

    refactor: processor uses rotate for binary left circular shift

--- lib/processor.rb
+++ lib/processor.rb
@@ -2,12 +2,15 @@ require 'pry'
 
 class Processor
   def circular_left_shift(binary, n)
-    n.times { binary = binary[1..-1] + binary[0] }
-    binary
+    binary.chars.rotate!(n).join
   end
 
   def bitwise_exclusive_or(arr)
+    return arr.shift if arr.length == 1
     # need to totally rework this
-    '0' + (arr[0].to_i(2) ^ arr[1].to_i(2) ^ arr[2].to_i(2) ^ arr[3].to_i(2)).to_s(2)
+    # '0' + (arr[0].to_i(2) ^ arr[1].to_i(2) ^ arr[2].to_i(2) ^ arr[3].to_i(2)).to_s(2)
+
+    left = arr.shift
+    right = bitwise_exclusive_or(arr)
   end
 end
